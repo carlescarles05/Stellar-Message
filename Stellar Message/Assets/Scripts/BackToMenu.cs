d952ed8423afef4994b1588b563951652f7dfbf8
carlescarles05 <151786310+carlescarles05@users.noreply.github.com>
2024-05-16 18:43:41 +0200

    BackToMenu

--- Stellar Message/Assets/Scripts/BackToMenu.cs
+++ Stellar Message/Assets/Scripts/BackToMenu.cs
@@ -5,8 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class BackToMenu : MonoBehaviour
 {
+
+    private void Start()
+    {
+        // Desbloquea y muestra el cursor
+        Cursor.lockState = CursorLockMode.None;
+    }
     public void Menu()
     {
+        
         SceneManager.LoadScene("menu");
     }
 }
--- Stellar Message/Assets/Scripts/MainMenu.cs
+++ Stellar Message/Assets/Scripts/MainMenu.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
+
+
     public void Play()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         SceneManager.LoadScene("SampleScene");
     }
     public void Exit()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         Application.Quit();
     }
 }
